553fb4d623ffff3c84349efb5845f3c9ec443b2b
Favour Adebose <favouradebose@gmail.com>
2024-06-26 10:12:06 +0100

    Update task1

--- task1_greeting/settings.py
+++ task1_greeting/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7)bvv)f5=%os79loszg)1@^(yd7#nk1#!n0u4$(n@2&icpx^x2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
