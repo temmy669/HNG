2954a81ffb1657c4d03fc02545a0d8532ebcff57
Favour Adebose <favouradebose@gmail.com>
2024-07-04 17:07:43 +0100

    secured API key

--- endPoint/views.py
+++ endPoint/views.py
@@ -1,5 +1,11 @@
 from django.http import JsonResponse
 import requests
+import os
+import logging
+from decouple import config
+
+# Configure logging
+logger = logging.getLogger(__name__)
 
 def hello(request):
     visitor_name = request.GET.get('visitor_name', 'Guest')
@@ -17,20 +23,20 @@ def hello(request):
             if loc:
                 lat, lon = loc.split(',')
     except Exception as e:
-        pass
+        logger.error(f"Error fetching IP info: {e}")
 
     # Get the weather information based on the location
     temperature = "unknown"
     if lat and lon:
-        weather_api_key = 'cc68d14a23ffbd9420015492e9828b3d'
+        weather_api_key = config('WEATHER_API_KEY')
         weather_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={weather_api_key}'
         try:
             weather_response = requests.get(weather_url)
             if weather_response.status_code == 200:
                 weather_data = weather_response.json()
-                temperature = weather_data['main']['temp']
+                temperature = weather_data['main'].get('temp', 'unknown')
         except Exception as e:
-            pass
+            logger.error(f"Error fetching weather data: {e}")
 
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
 
